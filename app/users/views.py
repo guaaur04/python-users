eaed217e6ae9a9ae0ea4e52952f782cadcfe465d
V Guardado <auricia.guardado@gmail.com>
2021-04-02 16:26:10 -0700

    First commit

---
+++ app/users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    response = """
+    <h1> Hello! This is my first Djano application.</h1>
+
+    <ul>
+        <li>This is a list item!</li>
+        <li> This is the second list item!</li>
+    </ul>
+
+    """
+
+    return HttpResponse(response)
+
+def detail(request):
+    return HttpResponse("<h1>This is the detail view!</h1>")
+
+def add(request):
+    return HttpResponse("<h1>This is the add")
+
